f28e213a9d50982c7d85e0a64a4349f2b4788730
Matt Courtney <macourtney@gmail.com>
2011-08-16 10:53:52 -0400

    Updated the project to my repository.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-record "1.0.2"
-  :description "A pseudo-port of ActiveRecord to the Clojure programming language"
-  :url "http://github.com/duelinmarkers/clj-record"
+(defproject org.clojars.macourtney/clj-record "1.0.2"
+  :description "A pseudo-port of ActiveRecord to the Clojure programming language. This is a non-snapshot version of clj-record for use in production projects."
+  :url "https://github.com/macourtney/clj-record"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
